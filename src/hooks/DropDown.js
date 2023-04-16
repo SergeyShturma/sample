d4c4e9ee51096cbde77afd769ad18a88012bd4a2
SergeyShturma <sergeyshturma@gmail.com>
2023-04-17 00:13:32 +0300

    unfix bg

--- src/hooks/DropDown.js
+++ src/hooks/DropDown.js
@@ -1,11 +1,17 @@
 import s from './DropDown.module.css';
 
 const DropDown = ({ setSortDir }) => (
-  <select className={s.select} onChange={e => setSortDir(e.target.value)}>
-    <option value="default">за популярністю </option>
-    <option value="lowtohigh">ціна: low to high </option>
-    <option value="hightolow">ціна: high to low </option>
-  </select>
+  <div className={s.dropdown_wrapper}>
+    <select
+      className={s.dropdown_select}
+      onChange={e => setSortDir(e.target.value)}
+    >
+      <option value="default">за популярністю </option>
+      <option value="lowtohigh">ціна: low to high </option>
+      <option value="hightolow">ціна: high to low </option>
+    </select>
+    <p className={s.dropdown_text}>*Ціни та наявність актуальні.</p>
+  </div>
 );
 
 export default DropDown;
