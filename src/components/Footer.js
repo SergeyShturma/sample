0db0c515923271695df00040b76db8a1bf59ca31
SergeyShturma <sergeyshturma@gmail.com>
2023-04-16 20:37:37 +0300

    added color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,7 @@ export const Footer = () => {
       <div className={s.footer_section}>
         <p className={s.footer_text}> 2023 | All Rights Reserved |&nbsp;</p>
         <p className={s.footer_margin}>
-          Developed by Shturma
-          {/* with */}
-          {/* <svg
-            className={s.footer_icon}
-            lang="en"
-            aria-label="heart"
-            width="14"
-            height="13"
-          >
-            <use href="/src/images/sprite.svg#icon-heart"></use>
-          </svg> */}
+          Developed by <span className={s.footer_name}>Shturma</span>
         </p>
       </div>
     </footer>
--- src/components/RunningString.js
+++ src/components/RunningString.js
@@ -4,7 +4,7 @@ import s from './RunningString.module.css';
 export default function RunningString() {
   return (
     <div className={s.string}>
-      <Marquee speed={45} gradientColor={[255, 255, 200]}>
+      <Marquee speed={45} gradientColor={[169, 223, 191]}>
         &nbsp;За рекомендацією від друга - знижка 10% на першу футболку!&nbsp;
       </Marquee>
     </div>
